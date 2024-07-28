5534c07bc4302cf37497cef42561f2c0220681b1
Saumilya JJ <82203974+saumilyagupta@users.noreply.github.com>
2024-07-28 22:08:41 +0530

    Update settings.py

--- FinanceApp/settings.py
+++ FinanceApp/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=$$$s+wd26@7@l+)dm26z%%p@&l*)otwuk$0+@mi^)6t$i*+61'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
